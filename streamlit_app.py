dd00bfcaf0d7c9f939128f02dcb7aada954da705
JohannGlsr <122325236+JohannGlsr@users.noreply.github.com>
2023-05-01 23:27:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,28 @@
 import requests
 import streamlit as st
 
+# Définir la couleur de fond
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #f0f5f5;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # URL de votre fonction Azure
 azure_function_url = "https://functionappforserverlessapp.azurewebsites.net/api/httptriggernofonction?user_id="
+
+# Ajouter un titre à l'application
+st.title("Système de recommandation d'articles")
 
 # Créer une zone de saisie pour l'ID utilisateur
 user_id = st.text_input("Entrez votre ID utilisateur")
 
-# Envoyer une requête HTTP à la fonction Azure lorsque l'utilisateur appuie sur le bouton "Recommander"
+# Ajouter un bouton pour envoyer la requête
 if st.button("Recommander"):
     # Ajouter l'ID utilisateur à la requête
     params = {"user_id": user_id}
@@ -18,7 +33,6 @@ if st.button("Recommander"):
     # Afficher la réponse de la fonction Azure dans l'interface utilisateur
     if response.status_code == 200:
         recommended_articles = response.text
-        st.success(f"Recommandations pour l'utilisateur {user_id}: {recommended_articles}")
+        st.success(f"Articles recommandés pour l'utilisateur {user_id}: {recommended_articles}")
     else:
         st.error("Erreur lors de la récupération des recommandations.")
-
